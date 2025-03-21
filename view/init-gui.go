b5d98abe2f43469b61a69c2baa0fe56dbc1e476e
Strocs <strocsdev@gmail.com>
2025-03-21 18:39:26 -0300

    add system tray app file

--- view/init-gui.go
+++ view/init-gui.go
@@ -1,34 +1,82 @@
 package view
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/driver/desktop"
+	"fastclick/settings"
+	"os"
+	"time"
+
+	"fyne.io/systray"
 )
 
-func InitGUI() {
-	a := app.New()
-	initSystemTray(a)
-	a.Run()
+const (
+	YELLOW_ICON_PATH = "d:\\Documents\\Dev\\fastclick\\assets\\yellow_icon.ico"
+	GREEN_ICON_PATH  = "d:\\Documents\\Dev\\fastclick\\assets\\green_icon.ico"
+	MENU_TOOLTIP     = "Click to change the Toggle Key"
+)
+
+func InitSysTrayApp() {
+	systray.Run(onReady, onExit)
 }
 
-func initSystemTray(a fyne.App) {
-	if desk, ok := a.(desktop.App); ok {
-		menu := createMenu("FastClick", []*fyne.MenuItem{
-			addMenuItem("Toggle Key", openToggleKeyModal),
-		})
-		desk.SetSystemTrayMenu(menu)
+func onReady() {
+	systray.SetIcon(getIcon(YELLOW_ICON_PATH))
+	systray.SetTooltip(MENU_TOOLTIP)
+
+	setToggleKeyMenu()
+	systray.AddSeparator()
+	setQuitMenu()
+}
+
+func addEvent(menuItem *systray.MenuItem, callback func()) {
+	for range menuItem.ClickedCh {
+		callback()
 	}
 }
 
-func createMenu(name string, items []*fyne.MenuItem) *fyne.Menu {
-	return fyne.NewMenu(name, items...)
+func setQuitMenu() {
+	mQuit := systray.AddMenuItem("Quit", "Close the application")
+
+	go addEvent(mQuit, systray.Quit)
 }
 
-func addMenuItem(name string, action func()) *fyne.MenuItem {
-	return fyne.NewMenuItem(name, action)
+func setToggleKeyMenu() {
+	currentSetting, err := settings.LoadSettings()
+	if err != nil {
+		panic(err)
+	}
+
+	mToggle := systray.AddMenuItem("Toggle Key: "+currentSetting.TriggerKey, MENU_TOOLTIP)
+
+	go addEvent(mToggle, func() { onToggleKeyMenuClicked(mToggle) })
 }
 
-func openToggleKeyModal() {
+func onToggleKeyMenuClicked(menuItem *systray.MenuItem) {
+	systray.SetIcon(getIcon(GREEN_ICON_PATH))
 
+	menuItem.SetTooltip("Press a key")
+
+	// Wait for the user to press a key
+	time.Sleep(800 * time.Millisecond) // Simulate user interaction
+	newKey := "F9"
+
+	// Update settings with the new key
+	newSetting, err := settings.UpdateSettings(newKey)
+	if err != nil {
+		panic(err)
+	}
+
+	menuItem.SetTitle("Toggle Key: " + newSetting.TriggerKey)
+	menuItem.SetTooltip(MENU_TOOLTIP)
+	systray.SetIcon(getIcon(YELLOW_ICON_PATH))
+}
+
+func onExit() {
+}
+
+func getIcon(path string) []byte {
+	icon, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return icon
 }
